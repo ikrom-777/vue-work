b968a97d5056a91ecfd67dc0e641d4ea115780db
Ikrom Najmiddinov <ikromnajmiddinov55657@gmail.com>
2022-12-11 17:03:04 +0500

    third

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <CreatePost/>
+    <PostList/>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreatePost,
+    PostList,
   }
 }
 </script>
